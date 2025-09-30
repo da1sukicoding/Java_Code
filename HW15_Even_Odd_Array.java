c6c394f7f363942f4927b0875e8b2e5ac4ee37b3
da1sukicoding <daisukicodingzx@gmail.com>
2025-09-30 22:43:22 +0700

    Add files via upload

--- HW15_Even_Odd_Array.java
+++ HW15_Even_Odd_Array.java
@@ -1,4 +1,4 @@
-public class HW15_Even_Odd {
+public class HW15_Even_Odd_Array {
     public static void main(String[] args) {
         int i,evencount=0,oddcount=0;
         int x[] = { 7, 9, -1, 4, 12, 9, 3, 2, -7, 2, 1, 9, -15, 24, 11, 13} ;
@@ -10,8 +10,12 @@ public class HW15_Even_Odd {
                 oddcount++;
             }      
         }
+
+            System.out.println("======================================");
             System.out.println("Sum of Even in array = " + evencount);
             System.out.println("Sum of Odd in array = " + oddcount);
+            System.out.println("======================================");
+            
         for (i = 0;i < x.length;i++) {
             if (x[i] % 2 == 0) {
                 System.out.println("index Even Is "+x[i]);
